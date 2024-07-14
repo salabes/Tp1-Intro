adb12f17df421b4116bb81a6c6d563b298b1b817
Nicolas Cazal <nicolas.cazal02@gmail.com>
2024-07-13 21:20:10 -0300

    cambio la estructura de la base de datos

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,3 @@
-import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,27 +5,27 @@ db = SQLAlchemy()
 class Jugadores(db.model):
     __tablename__ = 'jugadores'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String[255], nullable=False)
-    apellido = db.Column(db.String[255], nullable=False)
     equipo_id = db.Column(db.Integer, db.ForeignKey('equipos.id'))
     posicion_id = db.Column(db.Integer, db.ForeignKey('posiciones.id'))
-    formacion_id = db.Column(db.Integer, db.ForeignKey('formaciones.id'))
+    ubicacion_id = db.Column(db.Integer, db.ForeignKey('ubicaciones.id'))
+    nombre = db.Column(db.String[255], nullable=False)
+    apellido = db.Column(db.String[255], nullable=False)
     edad = db.Column(db.Integer, nullable=False)
     pie = db.Column(db.String[255], nullable=False)
 
 class Equipos(db.model):
     __tablename__ = 'equipos'
     id = db.Column(db.Integer, primary_key=True)
+    formacion_id = db.Column(db.Integer, db.ForeignKey('formaciones.id'))
     nombre = db.Column(db.String[255], nullable=False)
 
 class Formaciones(db.model):
     __tablename__ = 'formaciones'
     id = db.Column(db.Integer, primary_key=True)
     tipo_formacion_id = db.Column(db.Integer, db.ForeignKey('tipo_formaciones.id'))
-    equipo_id = db.Column(db.Integer, db.ForeignKey('equipos.id'))
 
 class Tipo_Formaciones(db.model):
-    __tablename__ = 'formaciones'
+    __tablename__ = 'tipo_formaciones'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String[255], nullable=False)
 
@@ -35,4 +34,9 @@ class Posiciones(db.model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String[255], nullable=False)
 
-
+class Ubicaciones(db.model):
+    __tablename__ = 'ubicaciones'
+    id = db.Column(db.Integer, primary_key=True)
+    posicion_id = db.Column(db.Integer, db.ForeignKey('posiciones.id'))
+    tipo_formacion_id = db.Column(db.Integer, db.ForeignKey('tipo_formaciones.id'))
+    lugar_en_la_formacion = db.Column(db.Integer, nullable=False)
