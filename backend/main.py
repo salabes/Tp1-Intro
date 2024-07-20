08301d4935c43903679169dd7cf39f526a989ba5
Nicolas Cazal <nicolas.cazal02@gmail.com>
2024-07-20 13:46:23 -0300

    agrego endpoint que devuelve los jugadores de la formacion de un equipo

--- backend/main.py
+++ backend/main.py
@@ -25,6 +25,31 @@ def get_equipos():
     except:
         return jsonify({'mensaje': 'No hay equipos'}), 500
 
+@app.route('/equipo/<id_equipo>/formacion', methods=['GET'])
+def get_jugadores_en_formacion(id_equipo):
+    try:
+        jugadores = Jugador.query.filter(
+            Jugador.equipo_id == id_equipo,
+            Jugador.lugar_en_formacion != None
+        ).all()
+    
+        jugadores_data = []
+        for jugador in jugadores:
+            jugador_data = {
+                'id': jugador.id,
+                'equipo_id': jugador.equipo_id,
+                'posicion_id': jugador.posicion_id,
+                'nombre': jugador.nombre,
+                'apellido': jugador.apellido,
+                'edad': jugador.edad,
+                'pierna_habil': jugador.pierna_habil,
+                'lugar_en_formacion': jugador.lugar_en_formacion
+            }
+            jugadores_data.append(jugador_data)
+        return jsonify(jugadores_data)
+    except:
+        return jsonify({'mensaje': 'No hay jugadores en la formación'}), 500
+
 if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
